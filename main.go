a38f8838d54e404db7fd097149fdee43fd789cd5
kulkarnivedant <vedant@golain.io>
2024-11-14 12:18:12 +0530

    got working !

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -13,6 +15,9 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	v1 "google.golang.org/grpc/reflection/grpc_reflection_v1"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/descriptorpb"
 )
 
 func main() {
@@ -24,10 +29,11 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	testService := &server.MyTestService{}
-
 	service_proto.RegisterTestServiceServer(grpcServer, testService)
 	syncService := &server.MySyncService{}
 	service_proto.RegisterSyncServiceServer(grpcServer, syncService)
+
+
 	reflection.Register(grpcServer)
 
 	go createClient()
@@ -58,23 +64,55 @@ func createClient() {
 	}
 
 
-	// Send the initial request to get the list of services
-	if err := stream.Send(&v1.ServerReflectionRequest{
-		MessageRequest: &v1.ServerReflectionRequest_FileContainingSymbol{
-			FileContainingSymbol: "reflect.TestService[Test]",
+	request:=&v1.ServerReflectionRequest{
+		MessageRequest: &v1.ServerReflectionRequest_FileByFilename{
+			FileByFilename: "reflect.proto",
 		},
-	}); err != nil {
+	}
+	// Send the initial request to get the list of services
+	if err := stream.Send(request); err != nil {
 		log.Fatalf("failed to send reflection request: %v", err)
 	}
 
-	// Receive the response and print the list of services
+	// Receive the response, json encode it, and print the list of services
 	response, err := stream.Recv()
 	if err != nil {
 		log.Fatalf("failed to receive reflection response: %v", err)
 	}
-	fmt.Println(response.GetFileDescriptorResponse())
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		log.Fatalf("failed to marshal reflection response to JSON: %v", err)
+	}
+	fmt.Println("JSON Response:", string(jsonResponse))
 
+	fmt.Println("================================================")
+	fmt.Println(response.GetFileDescriptorResponse().String())
+	fmt.Println("================================================")
 	
+	// Get the file descriptor proto bytes
+	fileDescBytes := response.GetFileDescriptorResponse().GetFileDescriptorProto()[0]
+	
+	// Create a new FileDescriptorProto
+	fileDesc := &descriptorpb.FileDescriptorProto{}
+	
+	// Unmarshal the bytes into the FileDescriptorProto
+	if err := proto.Unmarshal(fileDescBytes, fileDesc); err != nil {
+		log.Fatalf("failed to unmarshal file descriptor: %v", err)
+	}
+	
+	// Convert to JSON
+	jsonBytes, err := protojson.Marshal(fileDesc)
+	if err != nil {
+		log.Fatalf("failed to marshal to JSON: %v", err)
+	}
+	
+	// Print the JSON in a pretty format
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, jsonBytes, "", "  "); err != nil {
+		log.Fatalf("failed to indent JSON: %v", err)
+	}
+	fmt.Println("File Descriptor as JSON:")
+	fmt.Println(prettyJSON.String())
 
 	fmt.Println("Services provided by the gRPC server:")
 
